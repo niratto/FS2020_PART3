9beeb09a11cb3c8a97f2ad96435885f2a394ec59
niratto <niko.vihervuori@helsinki.fi>
2020-02-08 16:18:25 +0200

    tasks 3.13,.14,.15,.16,.18... .17 backend implemented already (time consumed: 2h 15mins)

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-//const util = require('util');
+// `mongodb+srv://nixon:nixon_fs2020@cluster0-kcv89.mongodb.net/phonebook?retryWrites=true&w=majority`
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
+
+const Number = require('./models/phonebook')
 
 let persons = [
     {
@@ -39,38 +42,63 @@ app.get('/', (req, res) => {
 
 // SHOW AMOUNT OF CONTACTS
 app.get('/info', (req, res) => {
-    let myTime = new Date();
-    let myUTCTime = myTime.toString();
-    let html = "<p>Phonebook has info for " + persons.length + " people</p>"
-    html += "<p>" + myUTCTime + "</p>"
-    res.send(html)
+    //console.log(util.inspect(req)); // Will give you more details than console.log
+    Number.find({}).then(result => {
+        let myTime = new Date();
+        let myUTCTime = myTime.toString();
+        let html = "<p>Phonebook has info for " + result.length + " people</p>"
+        html += "<p>" + myUTCTime + "</p>"
+        res.send(html)
+    
+    })
+    ////
 })
 
 // SHOW ALL
 app.get('/api/persons', (req, res) => {
     //console.log(util.inspect(req)); // Will give you more details than console.log
-    res.json(persons)
+    console.log(Number)
+    Number.find({}).then(result => {
+        res.json(result.map(number => number.toJSON()))
+    });
 })
 
 // SHOW A CONTACT THAT IS MATCHING GIVEN ID
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Number.findById(request.params.id).then(number => {
+        if (number) {
+            response.json(number.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    })
+        .catch(error => next(error))
 })
 
 // DELETE A CONTACT BY ID
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Number.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const number = {
+        name: body.name,
+        number: body.number
+    }
+
+    console.log(number)
+
+    Number.findByIdAndUpdate(request.params.id, number, { new: true })
+        .then(updatedNumber => {
+            response.json(updatedNumber.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 // ADD NEW CONTACT
@@ -91,18 +119,38 @@ app.post('/api/persons', (request, response) => {
         }
     })
 
-    const person = {
+    const number = new Number({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    number.save().then(num => {
+        response.json(num.toJSON())
+    })
 })
 
-const PORT = process.env.PORT || 3001
+/////////////////
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+/////////////////
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
