393f5cbb38175d2d81b11b15a748f96192347bcc
niratto <niko.vihervuori@helsinki.fi>
2020-02-09 23:30:06 +0200

    build

---
+++ build/precache-manifest.67652ba43e6c9e671197620c70f86e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eba4216357bb0877b61b51f94799fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98faf6aee9cf7281d9e5",
+    "url": "/static/css/main.3980f55b.chunk.css"
+  },
+  {
+    "revision": "4b18cf36501447135d88",
+    "url": "/static/js/2.7c9eebd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c9eebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98faf6aee9cf7281d9e5",
+    "url": "/static/js/main.2622dc08.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
